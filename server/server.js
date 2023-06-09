b495682abf1694ad0aeafee6695b262e618ab37c
elciddelapaz <elcid.delapaz@gmail.com>
2023-06-09 13:41:02 +0800

    added cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const routes = require('./routes/routes')
 const mongoose = require('mongoose')
 
+app.use(cors())
 app.use(express.json())
 
 app.use((req, res, next) => {
